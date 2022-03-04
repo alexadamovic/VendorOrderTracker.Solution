770b89877521f012ace9bd83a0cd5e45d6fee297
Alex Adamovic <alexanderadamovic@gmail.com>
2022-03-04 13:53:23 -0800

    add VendorController and vendor cshtml views

--- VendorOrderTracker/Startup.cs
+++ VendorOrderTracker/Startup.cs
@@ -37,7 +37,7 @@ namespace ToDoList
       
       app.Run(async (context) =>
       {
-        await context.Response.WriteAsync("Hello World!");
+        await context.Response.WriteAsync("Pardon. Something is fishy with the controllers.");
       });
     }
   }
